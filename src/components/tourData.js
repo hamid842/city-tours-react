cd9e071834cfe3ad65657c9ce7e0248d095de659
hamid842 <h_mohammadi842@yahoo.com>
2019-12-16 23:48:38 +0330

    First commit

---
+++ src/components/tourData.js
@@ -0,0 +1,30 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york tour",
+    info: "something"
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris tour",
+    info: "something"
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london tour",
+    info: "something"
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo tour",
+    info: "something"
+  }
+];
